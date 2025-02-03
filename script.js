f33601995052820059982a79d4559762894f3620
olatona-enoch <enocholatona@gmail.com>
2025-02-03 07:53:09 +0100

    added a second style and made the dark mode theme

--- script.js
+++ script.js
@@ -16,38 +16,52 @@ setInterval(execute,0);
 // let firstsection = document.getElementsByClassName("firstsection")
 // firstsection.style(backgroundcolor = 'red')
 
+// function theme() {
+//     const theme = document.getElementById("theme");
+//     console.log('pressed')
+//     if(document.getElementById("theme").innerHTML == "Dark Mode"){
+//         document.getElementById("theme").innerHTML ="Light Mode"
+//         document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+
+//         const paragraph = document.getElementsByTagName("p");
+//         for (let i = 0; i < paragraph.length; i++) {
+//         paragraph[i].style.color = "#777"  
+//         }
+
+//         const headertwo = document.getElementsByTagName("h2");
+//         for (let i = 0; i < headertwo.length; i++) {
+//         headertwo[i].style.color = "black"  
+//         }
+//     } else{
+//         // document.getElementsByClassName('top-buttons').innerHTML = "Light Mode";
+//         document.getElementById("theme").innerHTML = "Dark Mode";
+//         document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+
+//         const paragraph = document.getElementsByTagName("p");
+//         for (let i = 0; i < paragraph.length; i++) {
+//         paragraph[i].style.color = "white"  
+//         }
+
+//         const headertwo = document.getElementsByTagName("h2");
+//         for (let i = 0; i < headertwo.length; i++) {
+//         headertwo[i].style.color = "white"  
+//         }
+//     }
+    
+//     document.getElementsByTagName("h1")[0].style.backgroundColor = "r";
+   
+// }
+
 function theme() {
     const theme = document.getElementById("theme");
     console.log('pressed')
     if(document.getElementById("theme").innerHTML == "Dark Mode"){
         document.getElementById("theme").innerHTML ="Light Mode"
-        document.getElementsByTagName("body")[0].style.backgroundColor = "white";
 
-        const paragraph = document.getElementsByTagName("p");
-        for (let i = 0; i < paragraph.length; i++) {
-        paragraph[i].style.color = "#777"  
-        }
-
-        const headertwo = document.getElementsByTagName("h2");
-        for (let i = 0; i < headertwo.length; i++) {
-        headertwo[i].style.color = "black"  
-        }
     } else{
         // document.getElementsByClassName('top-buttons').innerHTML = "Light Mode";
         document.getElementById("theme").innerHTML = "Dark Mode";
-        document.getElementsByTagName("body")[0].style.backgroundColor = "black";
-
-        const paragraph = document.getElementsByTagName("p");
-        for (let i = 0; i < paragraph.length; i++) {
-        paragraph[i].style.color = "white"  
-        }
-
-        const headertwo = document.getElementsByTagName("h2");
-        for (let i = 0; i < headertwo.length; i++) {
-        headertwo[i].style.color = "white"  
-        }
-    }
-    
-    document.getElementsByTagName("h1")[0].style.backgroundColor = "r";
-   
+    } 
+    document.body.classList.toggle("dark-mode");
 }
+
